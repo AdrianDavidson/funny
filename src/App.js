3c824769f652882b93ceee6b098cf3c06fe45abd
Adrian Davidson <31959515+AdrianDavidson@users.noreply.github.com>
2023-02-09 09:59:56 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "../src/img/hand.png";
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={logo} className= "hand" alt="gotcha" width="500" height="600"></img>
+      <h1>Gotcha</h1>
+    </div>
+  );
+}
+
+export default App;
